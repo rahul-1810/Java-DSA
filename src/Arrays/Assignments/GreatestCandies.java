43f81e5fc87494e40f2541804398d63b52efab2d
Rahul Kashyap <rahul181020@gmail.com>
2021-09-23 23:51:19 +0530

    array ques done

---
+++ src/Arrays/Assignments/GreatestCandies.java
@@ -0,0 +1,25 @@
+package Arrays.Assignments;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GreatestCandies {
+    public static void main(String[] args) {
+        int[] candies = {2,3,5,1,3};
+        int extraCandies = 3;
+       List<Boolean> ans = kidsWithCandies(candies,extraCandies);
+        System.out.println(ans);
+    }
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<>(candies.length);
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<candies.length;i++){
+            max = Math.max(max,candies[i]);
+        }
+        for(int i: candies){
+            ans.add(i >= max - extraCandies);
+        }
+        return ans;
+    }
+}
